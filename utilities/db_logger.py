136bbdbdc377eef513635eb26673169d6b04572a
RajeshThakur1 <rajesh@isl.net.in>
2022-04-10 13:35:21 +0530

    added the last commit.

--- utilities/db_logger.py
+++ utilities/db_logger.py
@@ -4,3 +4,12 @@ __created_date__ = "10-04-2022"
 
 
 import pymongo
+import logging
+
+import config as cfg
+
+logger = logging.getLogger(__name__)
+
+myclient = pymongo.MongoClient(cfg.MONGO_DB_URL)
+mydb = myclient[cfg.DB_NAME]
+
