68e1979717848d11fd522325a668b0de88629261
RajeshThakur1 <rajesh@isl.net.in>
2022-04-10 13:23:51 +0530

    chenge DB name

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ import os
 instance_type = os.environ['INSTANCE_TYPE']
 
 if instance_type == "PRODUCTION":
-    DB_NAME = "prod_db"
+    DB_NAME = "pybron_Faculties"
 elif instance_type == "STAGE":
-    DB_NAME = "stage_db"
+    DB_NAME = "pybron_Faculties"
 elif instance_type == "DEV":
-    DB_NAME = "dev_db"
+    DB_NAME = "pybron_Faculties"
 
 MONGO_DB_URL = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 COLLECTION_NAME = "pybron"
