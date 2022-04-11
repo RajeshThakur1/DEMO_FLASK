e108eaff16031a9f5b58a7fbd4c98d8849543d3b
RajeshThakur1 <rajesh@isl.net.in>
2022-04-11 21:16:19 +0530

    upserted the data successfully

--- app.py
+++ app.py
@@ -61,6 +61,26 @@ def insert_faculty():
     return jsonify(all_utils.response_format({"status_code":200,"message":"data inserted Successfully"}))
 
 
+@app.route('/upsert_faculty',methods=["POST"])
+@cross_origin(origin="*")
+def upsert_faculty():
+    logger.info("Inserting the new faculty in pybron faculty")
+    com_name = request.json['companyName']
+    logger.info(f"company name {com_name}")
+    faculty_name = request.json['faculty_name']
+    logger.info(f"faculty name {faculty_name}")
+    specilization = request.json['specilization']
+    logger.info(f"specilization {specilization}")
+    price = request.json['price']
+    logger.info(f"price {price}")
+    obj = {"companyName": com_name, "faculty_name": faculty_name, "specilization": specilization, "price": price}
+    collection_name = cfg.COLLECTION_NAME
+    db_logger.upsert_obj(obj=obj, collection_name=collection_name,faculty_name="romio")
+    return jsonify(all_utils.response_format({"status_code":200,"message":"data upserted Successfully"}))
+
+
+
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=False,port=cfg.PORT)
